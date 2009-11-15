de90613a8d821a4e6cff19da27b60f705f47b0ef
clutchski <clutchski@gmail.com>
2009-11-14 22:21:46 -0500

    refactored Caribou to support a command line interface

--- caribou.py
+++ caribou.py
@@ -69,11 +69,14 @@ class Migration(object):
     def __init__(self, path):
         self.path = path
         self.filename = os.path.basename(path)
-        self.name, _ = os.path.splitext(self.filename)
+        self.module_name, _ = os.path.splitext(self.filename)
         # call get_version, will assert the filename works
         self.get_version()
+        self.name = self.module_name[UTC_LENGTH:]
+        while self.name.startswith('_'):
+            self.name = self.name[1:]
         try:
-            self.module = imp.load_source(self.name, path)
+            self.module = imp.load_source(self.module_name, path)
         except:
             m = "invalid migration [%s]: %s" % (path, traceback.format_exc())
             raise InvalidMigrationError(m)
@@ -106,87 +109,129 @@ class Migration(object):
     def __repr__(self):
         return 'Migration(%s)' % self.filename
 
+class Database(object):
 
-def get_migrations(directory, reverse=False):
-    """ return the migrations in the directory, sorted by age"""
+    def __init__(self, db_url):
+        self.db_url = db_url
+        self.conn = sqlite3.connect(db_url)
+
+    def get_version(self):
+        sql = "SELECT version FROM %s" % VERSION_TABLE
+        try:
+            with execute(self.conn, sql) as cursor:
+                return cursor.fetchall()[0][0]
+        except sqlite3.OperationalError:
+            return None
+
+    def is_version_controlled(self):
+        sql = """SELECT *
+                   FROM sqlite_master
+                  WHERE type = 'table'
+                    AND name = :1"""
+        with execute(self.conn, sql, [VERSION_TABLE]) as cursor:
+            return bool(cursor.fetchall())
+
+    def upgrade(self, migrations, target_version=None):
+        current_version = self.get_version()
+        for migration in migrations:
+            if migration.get_version() <= current_version:
+                continue
+            if target_version and migration.get_version() > target_version:
+                break
+            migration.upgrade(self.conn)
+            new_version = migration.get_version()
+            self.update_version(new_version)
+
+    def downgrade(self, migrations, target_version):
+        current_version = self.get_version()
+        for i, migration in enumerate(migrations):
+            if migration.get_version() > current_version:
+                continue
+            if migration.get_version() <= target_version:
+                break
+            migration.downgrade(self.conn)
+            # set the version to the one below on the queue, or 0 if 
+            # this is the last
+            next_version = 0
+            next_i = i + 1
+            if next_i < len(migrations):
+                next_migration = migrations[next_i]
+                next_version = next_migration.get_version()
+            self.update_version(next_version)
+
+    def get_version(self):
+        """ return the database's version, or None if it is not versioned """
+        sql = "SELECT version FROM %s" % VERSION_TABLE
+        if not self.is_version_controlled():
+            return None
+        with execute(self.conn, sql) as cursor:
+            return cursor.fetchall()[0][0]
+
+    def update_version(self, version):
+        sql = 'update %s set version = :1' % VERSION_TABLE
+        with transaction(self.conn):
+            self.conn.execute(sql, [version])
+
+    def initialize_version_control(self):
+        sql = """
+            CREATE TABLE IF NOT EXISTS %s
+            ( version TEXT )""" % VERSION_TABLE
+        with transaction(self.conn):
+            self.conn.execute(sql)
+            self.conn.execute('insert into %s values (0)' % VERSION_TABLE)
+    
+    def __repr__(self):
+        return 'Database("%s")' % self.db_url
+
+def migration_exists(migrations, version):
+    version_exists = False
+    for migration in migrations:
+        if version == migration.get_version():
+            version_exists = True
+            break
+    return version_exists
+
+def get_migrations(directory, target_version=None, reverse=False):
+    """
+    return the migrations in the directory, sorted by version number. if a
+    target version is passed, assert a migration with that version exists
+    """
     wildcard = os.path.join(directory, '*.py')
     migration_files = glob.glob(wildcard)
     migrations = [Migration(f) for f in migration_files]
+    if target_version and not migration_exists(migrations, target_version):
+        m = "no migration exists with version [%s]" % target_version
+        raise InvalidMigrationError(m)
     return sorted(migrations, key=lambda x: x.get_version(), reverse=reverse)
     
-def get_version(conn):
-    """ return the database's version, or None if it is not versioned """
-    sql = "SELECT version FROM %s" % VERSION_TABLE
-    try:
-        with execute(conn, sql) as cursor:
-            return cursor.fetchall()[0][0]
-    except sqlite3.OperationalError:
-        return None
-
-def is_version_controlled(conn):
-    sql = """SELECT *
-               FROM sqlite_master
-              WHERE type = 'table'
-                AND name = :1"""
-    with execute(conn, sql, [VERSION_TABLE]) as cursor:
-        return bool(cursor.fetchall())
-
-def update_version(conn, version):
-    sql = 'update %s set version = :1' % VERSION_TABLE
-    with transaction(conn):
-        conn.execute(sql, [version])
-
-def add_version_control(conn):
-    sql = """
-        CREATE TABLE IF NOT EXISTS %s
-        ( version TEXT )""" % VERSION_TABLE
-    with transaction(conn):
-        conn.execute(sql)
-        conn.execute('insert into %s values (0)' % VERSION_TABLE)
-
 def upgrade(db_url, migration_dir, version=None):
-    conn = sqlite3.connect(db_url)
-    if not is_version_controlled(conn):
-        add_version_control(conn)
-    current_version = get_version(conn)
-    migrations = get_migrations(migration_dir)
-    for migration in migrations:
-        if migration.get_version() <= current_version:
-            continue
-        if version and migration.get_version() > version:
-            break
-        migration.upgrade(conn)
-        update_version(conn, migration.get_version())
-
+    db = Database(db_url)
+    if not db.is_version_controlled():
+        db.initialize_version_control()
+    migrations = get_migrations(migration_dir, version)
+    db.upgrade(migrations, version)
 
 def downgrade(db_url, migration_dir, version):
-    conn = sqlite3.connect(db_url)
-    if not is_version_controlled(conn):
-        raise Error("%s is not under version control" % db_url)
-    current_version = get_version(conn)
-    migrations = get_migrations(migration_dir, reverse=True)
-    for i, migration in enumerate(migrations):
-        if migration.get_version() > current_version:
-            continue
-        if migration.get_version() <= version:
-            break
-        migration.downgrade(conn)
-        # set the version to the one below on the queue, or 0 if 
-        # this is the last
-        next_version = 0
-        next_i = i + 1
-        if next_i < len(migrations):
-            next_migration = migrations[next_i]
-            next_version = next_migration.get_version()
-        update_version(conn, next_version)
+    db = Database(db_url)
+    if not db.is_version_controlled():
+        m = "Can't downgrade %s because it is not version controlled." % db_url
+        raise Error(m)
+    target_version = version
+    if version == '0':
+        target_version = None
+    migrations = get_migrations(migration_dir, target_version, reverse=True)
+    db.downgrade(migrations, version)
 
-def get_next_version():
-    now = datetime.datetime.now()
-    return now.strftime("%Y%m%d%H%M%S")
+def get_version(db_url):
+    db = Database(db_url)
+    return db.get_version()
 
 def create_migration(name, directory=None):
     if not directory:
         directory = '.'
+    def get_next_version():
+        now = datetime.datetime.now()
+        return now.strftime("%Y%m%d%H%M%S")
     version = get_next_version()
     contents = MIGRATION_TEMPLATE % {'name':name, 'version':version}
     name = name.replace(' ', '_')
@@ -212,5 +257,4 @@ def downgrade(connection):
     # add your downgrade step here
     pass
 """
-
 
